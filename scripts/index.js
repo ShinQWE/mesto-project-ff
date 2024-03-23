019c62eb9048ab62b7fd087924bc50acafd0775a
Alex Mois <pro100pro20021@mail.ru>
2024-03-23 18:53:25 +1000

    qwe1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,50 @@
-// @todo: Темплейт карточки
+// template
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.places__item');
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+// Создание карточек
 
-// @todo: Вывести карточки на страницу
+const createCard = (data) => {
+   const templateElements = cardTemplate.cloneNode(true);
+   const cardTitle = templateElements.querySelector(".card__title");
+   const cardDelete = templateElements.querySelector(".card__delete-button");
+   const imgCard = templateElements.querySelector(".card__image");
+
+   imgCard.src = data.link;
+   cardTitle.textContent = data.name;
+
+   function ElementsCard() {
+      popupImage.src = imgCard.src;
+      popupImage.alt = cardTitle.textContent;
+      popupCaption.textContent = cardTitle.textContent;
+      openPopup(popupTypeImg);
+   }
+
+   imgCard.addEventListener("click", ElementsCard);
+   cardDelete.addEventListener("click", DeleteCard);
+   
+   return templateElements;
+};
+
+// Удаление карточек
+
+const DeleteCard = (evnt) => {
+   evnt.target.closest(".places__item").remove();
+};
+
+const renderCard = (data) => {
+   placesList.prepend(createCard(data));
+};
+
+// Добавление карточек из card.js
+
+initialCards.forEach((data) => {
+   renderCard(data);
+});
+
+
+
+
+
