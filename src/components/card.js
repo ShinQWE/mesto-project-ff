0715c0189eb35b0ef3361c57e51d6fcef7ae2742
Alex Mois <pro100pro20021@mail.ru>
2024-04-15 20:14:49 +1000

    добавление like

---
+++ src/components/card.js
@@ -0,0 +1,57 @@
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.places__item');
+const placesList = document.querySelector('.places__list');
+
+
+
+// Создание карточек
+
+const createCard = (data) => {
+   const cardElement = cardTemplate.cloneNode(true);
+   const cardTitle = cardElement.querySelector(".card__title");
+   const cardDelete = cardElement.querySelector(".card__delete-button");
+   const imgCard = cardElement.querySelector(".card__image");
+   const likeBtn = cardElement.querySelector('.card__like-button');
+
+   imgCard.src = data.link;
+   cardTitle.textContent = data.name;
+
+   cardDelete.addEventListener("click", deleteCard);
+   likeBtn.addEventListener("click", likeCard);
+  
+
+   
+   return cardElement;
+};
+
+// Удаление карточек
+
+const deleteCard = (evnt) => {
+   evnt.target.closest(".places__item").remove();
+};
+
+// Генерация карточек
+
+const renderCard = (data) => {
+   placesList.append(createCard(data));
+};
+
+// Лайк карточек
+
+const likeCard = (evnt) => {
+   const likeBtnEvn = evnt.target;
+   if (likeBtnEvn.classList.contains('card__like-button_is-active')) {
+      likeBtnEvn.classList.remove('card__like-button_is-active');
+      likeBtnEvn.style.background = 'url(./images/like-inactive.svg)';
+    } else {
+      likeBtnEvn.classList.add('card__like-button_is-active');
+      likeBtnEvn.style.background = 'url(./images/like-active.svg)';
+    }
+    console.log(likeCard);
+}; 
+
+
+
+export {createCard, deleteCard, renderCard, likeCard};
+
+
+
